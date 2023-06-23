51fa1869fd1868662205010afd925fc5eacb2c0f
Aakash-bhandari <102407592+Aakash-bhandari@users.noreply.github.com>
2023-06-23 23:39:31 +0530

    Dashborad Updated

--- src/components/Input/Index.js
+++ src/components/Input/Index.js
@@ -5,20 +5,21 @@ const Input = ({
   name = "",
   type = "text",
   className = "",
+  inputClassname='',
   isRequired = true,
   placeholder = "",
   value = '',
   onChange = () => { },
 }) => {
   return (
-    <div className="w-1/2">
+    <div className={`${className}`}>
       <label for={name} className="block text-sm font-medium text-gray-800">
         {lable}
       </label>
       <input
         type={type}
         id={name}
-        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  block w-full p-2.5 ${className}`}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  block w-full p-2.5 ${inputClassname}`}
         placeholder={placeholder}
         required={isRequired}
         onChange={onChange}
--- src/modules/Dashboard/index.js
+++ src/modules/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Avatar from "../../assets/avatar1.jpg";
-
+import Input from "../../components/Input/Index";
 const DashBoard = () => {
   const contacts = [
     {
@@ -20,11 +20,6 @@ const DashBoard = () => {
     },
     {
       name: "Yuvraj",
-      status: "Available",
-      img: Avatar,
-    },
-    {
-      name: "Kapil Dev",
       status: "Available",
       img: Avatar,
     },
@@ -65,8 +60,66 @@ const DashBoard = () => {
           </div>
         </div>
       </div>
-      <div className="w-[50%] border border-black h-screen"></div>
-      <div className="w-[25%] border border-black h-screen"></div>
+      <div className="w-[50%] h-screen bg-white flex flex-col items-center">
+        <div className="w-[75%] bg-secondary h-[80px] my-14 rounded-full flex items-center px-14 ">
+          <div className="cursor-pointer"> <img src={Avatar} width={50} heigth={50} /></div>
+          <div className="ml-6 mr-auto">
+            <h3 className="text-lg">Rijul</h3>
+            <p className="text-sm font-light text-gray-600">Online</p>
+          </div>
+          <div className="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone-outgoing" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+              <path d="M15 9l5 -5" />
+              <path d="M16 4l4 0l0 4" />
+            </svg>
+          </div>
+        </div>
+        <div className="h-[75%]  w-full overflow-scroll  shadow-md">
+          <div className=" p-14">
+            <div className="max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 m-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+            <div className="max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+            <div className="max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 m-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+            <div className="max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+            <div className="max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 m-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+            <div className="max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </div>
+          </div>
+        </div>
+        <div className="p-14 w-full flex items-center">
+          <Input placeholder="Type a message" className="w-[75%]" inputClassname="p-4 border-0 shadow-md rounded-full bg-light focus:ring-0 focus:border-0 outline-none" />
+          <div className="ml-4 p-2 cursor-pointer bg-light rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-send" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10 14l11 -11" />
+              <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+            </svg>
+          </div>
+          <div className="ml-4 p-2 cursor-pointer bg-light rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+              <path d="M9 12l6 0" />
+              <path d="M12 9l0 6" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-[25%] h-screen bg-light">
+
+      </div>
     </div>
   );
 };
