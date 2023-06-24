0e8e1c45f59167c89c538372f9b81ff9039013ee
tiwaririjul <tiwaririjul7@gmail.com>
2023-06-24 11:16:51 +0530

    protected route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import "./App.css";
 import DashBoard from "./modules/Dashboard";
 import Form from "./modules/Form";
+import { Routes, Route, Navigate } from "react-router-dom";
+const ProtectedRoutes = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("user : token") !== null || true;
+  if (!isLoggedIn && window.location.pathname === "/") {
+    return <Navigate to={`/users/sign_in`} />;
+  } else if (
+    isLoggedIn &&
+    ["/users/sign_in", "/users/sign_up"].includes(window.location.pathname)
+  ) {
+    return <Navigate to={"/"} />;
+  }
+
+  // if (!isLoggedIn) {
+  //   return <Navigate to={"/users/sign_in"} />;
+  // }
+
+  return children;
+};
 
 function App() {
   return (
-    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
-      {/* <Form /> */}
-      <DashBoard />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoutes>
+            <DashBoard />
+          </ProtectedRoutes>
+        }
+      />
+      <Route
+        path="/users/sign_in"
+        element={
+          <ProtectedRoutes>
+            <Form isSignInPage={true} />
+          </ProtectedRoutes>
+        }
+      />
+      <Route
+        path="/users/sign_up"
+        element={
+          <ProtectedRoutes>
+            <Form isSignInPage={false} />
+          </ProtectedRoutes>
+        }
+      />
+    </Routes>
+
+    // <div className="bg-[#e1edff] h-screen flex justify-center items-center">
+    //   {/* <Form /> */}
+    //   <DashBoard />
+    // </div>
   );
 }
 
--- src/modules/Form/index.js
+++ src/modules/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Input from "../../components/Input/Index";
 import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
 const Form = ({ isSignInPage = true }) => {
   const [data, setdata] = useState({
     ...Button(
@@ -11,59 +12,69 @@ const Form = ({ isSignInPage = true }) => {
     email: "",
     password: "",
   });
+
+  const navigate = useNavigate();
+
   return (
-    <div className="bg-white w-[450px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
-      <div className="text-4xl font-extrabold">
-        Welcome {isSignInPage && "Back"}
-      </div>
-      <div className=" text-xl font-light mb-14">
-        {isSignInPage ? "Sign in to get Explored" : "Sign up to get started"}
-      </div>
-      <form
-        className="flex flex-col items-center w-full"
-        onSubmit={() => console.log("submitted..")}
-      >
-        {!isSignInPage && (
+    <div className="bg-light h-screen flex justify-center items-center">
+      <div className="bg-white w-[500px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+        <div className="text-4xl font-extrabold">
+          Welcome {isSignInPage && "Back"}
+        </div>
+        <div className=" text-xl font-light mb-14">
+          {isSignInPage ? "Sign in to get Explored" : "Sign up to get started"}
+        </div>
+        <form
+          className="flex flex-col items-center w-full"
+          onSubmit={() => console.log("submitted..")}
+        >
+          {!isSignInPage && (
+            <Input
+              lable="Full Name"
+              name="name"
+              placeholder="Enter your full name"
+              className="mb-6 w-[50%]"
+              value={data.fullname}
+              onChange={(e) => setdata({ ...data, fullname: e.target.value })}
+            />
+          )}
           <Input
-            lable="Full Name"
-            name="name"
-            placeholder="Enter your full name"
-            className="mb-6"
-            value={data.fullname}
-            onChange={(e) => setdata({ ...data, fullname: e.target.value })}
+            lable="Email address"
+            name="email"
+            placeholder="Enter your email"
+            className="mb-6 w-[50%]"
+            value={data.email}
+            onChange={(e) => setdata({ ...data, email: e.target.value })}
           />
-        )}
-        <Input
-          lable="Email address"
-          name="email"
-          placeholder="Enter your email"
-          className="mb-6"
-          value={data.email}
-          onChange={(e) => setdata({ ...data, email: e.target.value })}
-        />
-        <Input
-          lable="password"
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          className="mb-12"
-          value={data.password}
-          onChange={(e) => setdata({ ...data, password: e.target.value })}
-        />
-        <Button
-          label={isSignInPage ? "Sign in" : " Sign up"}
-          className="w-1/3 mb-2"
-          type="submit"
-        />
-      </form>
-      <div>
-        {" "}
-        {isSignInPage
-          ? "Don't have an account ?"
-          : "Already have an accouny ?"}{" "}
-        <span className="text-primary cursor-pointer underline">
-          {isSignInPage ? "Sign up" : " Sign in"}
-        </span>
+          <Input
+            lable="password"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            className="mb-12 w-[50%]"
+            value={data.password}
+            onChange={(e) => setdata({ ...data, password: e.target.value })}
+          />
+          <Button
+            label={isSignInPage ? "Sign in" : " Sign up"}
+            className="w-1/2 mb-2"
+            type="submit"
+          />
+        </form>
+        <div>
+          {" "}
+          {isSignInPage
+            ? "Don't have an account ?"
+            : "Already have an accouny ?"}{" "}
+          <span
+            className="text-primary cursor-pointer underline"
+            onClick={() =>
+              navigate(`/users/${isSignInPage ? "sign_up" : "sign_in"}`)
+            }
+          >
+            {isSignInPage ? "Sign up" : " Sign in"}
+          </span>
+        </div>
       </div>
     </div>
   );
