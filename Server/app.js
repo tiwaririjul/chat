d812e4dc12adc5270cc82f7350262105610acf48
tiwaririjul <tiwaririjul7@gmail.com>
2023-06-25 17:14:58 +0530

    register login

--- Server/app.js
+++ Server/app.js
@@ -1,21 +1,90 @@
-const express = require('express');
+const express = require("express");
 //Import Files
-const Users= require('./models/Users');
+const Users = require("./models/Users");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 //Connection to Database
-require('./db/connection');
+require("./db/connection");
 
 //app use
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-
-const port= process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 //Routes
-app.get('/',(req,res)=>{
-    res.write('Welcome..')
+app.get("/", (req, res) => {
+  res.write("Welcome..");
 });
 
-app.listen(port,()=>{
-    console.log('Server is Listening on port '+ port);
-})
+app.post("/api/register", async (req, res, next) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      res.status(400).send("Please fill all require detail");
+    } else {
+      const isAlreadyExist = await Users.findOne({ email });
+      if (isAlreadyExist) {
+        res.status(400).send("user already exist");
+      } else {
+        const newUser = new Users({ fullName, email });
+        bcryptjs.hash(password, 10, (err, hashedPassword) => {
+          newUser.set("password", hashedPassword);
+          newUser.save();
+          next();
+        });
+        return res.status(200).send("user registered successfully");
+      }
+    }
+  } catch (error) {}
+});
+
+app.post("/api/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send("Please fill all required fields");
+    } else {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        res.status(400).send("User email or password is incorrect");
+      } else {
+        const validateUser = await bcryptjs.compare(password, user.password);
+        if (!validateUser) {
+          res.status(400).send("User email or password is incorrect");
+        } else {
+          const payload = {
+            userId: user.id,
+            email: user.email,
+          };
+
+          const JWT_SECRET_KEY =
+            process.env.JWT_SECRET_KEY || "THIS_IS_A_JWT_SECRET_KEY";
+          jwt.sign(
+            payload,
+            JWT_SECRET_KEY,
+            { expiresIn: 84600 },
+            async (err, token) => {
+              await Users.updateOne(
+                { _id: user._id },
+                {
+                  $set: { token },
+                }
+              );
+              user.save();
+              next();
+            }
+          );
+
+          res.status(200).json({
+            user,
+          });
+        }
+      }
+    }
+  } catch (error) {}
+});
+
+app.listen(port, () => {
+  console.log("Server is Listening on port " + port);
+});
--- Server/db/connection.js
+++ Server/db/connection.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const url =
+  "mongodb+srv://admin:admin@chatapp.jknjbsi.mongodb.net/?retryWrites=true&w=majority";
 
-const url='mongodb+srv://admin:admin@chatapp.jknjbsi.mongodb.net/?retryWrites=true&w=majority';
+//   "mongodb+srv://admin:<password>@chatapp.jknjbsi.mongodb.net/";
 
-mongoose.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Database Connected Sucessfully..");
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log("Some Error Occured While Connecting to DB");
-})
+  });
